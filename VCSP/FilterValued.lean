6e2e6383f6edb39a14310c0a7cd779b8f3045372
madvorak <dvorakmartinbridge@seznam.cz>
2023-12-27 18:02:47 +0100

    UpperSet instead of PFilter

--- VCSP/FilterValued.lean
+++ VCSP/FilterValued.lean
@@ -1,37 +1,39 @@
-import Mathlib.Order.PFilter
+import Mathlib.Order.UpperLower.Basic
 import Mathlib.Data.Set.Pointwise.Basic
 import VCSP.FractionalPolymorphisms
 
 
 abbrev FilterValuedCsp (D C : Type*) [OrderedAddCommMonoid C] :=
-  Set (Σ (n : ℕ), (Fin n → D) → Order.PFilter C)
+  Set (Σ (n : ℕ), (Fin n → D) → UpperSet C)
 
 
 variable {C : Type*} [OrderedAddCommMonoid C]
 
-def addMink_aux (x y : Order.Ideal C) : Order.Ideal C :=
-  let z := { a + b | (a ∈ x) (b ∈ y) }
-  have hz : Order.IsIdeal z := by
-    constructor
-    · sorry -- TODO `IsLowerSet`
-    · obtain ⟨x₁, hx₁⟩ := x.nonempty
-      obtain ⟨y₁, hy₁⟩ := y.nonempty
-      exact ⟨x₁ + y₁, x₁, hx₁, y₁, hy₁, rfl⟩
-    · sorry -- TODO `DirectedOn`
-  hz.toIdeal
+def addMink (x y : UpperSet C) : UpperSet C :=
+  ⟨{ a + b | (a ∈ x) (b ∈ y) }, by
+    intro c d cd hc
+    rw [Set.mem_setOf_eq] at hc ⊢
+    obtain ⟨a, ha, b, hb, eq_c⟩ := hc
+    -- ??
+    sorry⟩
 
-def addMink (x y : Order.PFilter C) : Order.PFilter C :=
-  ⟨addMink_aux x.dual y.dual⟩
+lemma addMink_right_comm : RightCommutative (@addMink C _) := by
+  intro x y z
+  simp [addMink]
+  apply Set.eq_of_subset_of_subset <;>
+  · intro d hd
+    rw [Set.mem_setOf_eq] at hd ⊢
+    obtain ⟨a, ha, b, hb, c, hc, eq_d⟩ := hd
+    use a, ha, c, hc, b, hb
+    rw [add_right_comm]
+    exact eq_d
 
-instance : Zero (Order.PFilter C) where
-  zero := ⟨⟨{0}, sorry⟩, Set.zero_nonempty, directedOn_singleton IsRefl.reflexive 0⟩
+def Multiset.sumMink (s : Multiset (UpperSet C)) : UpperSet C :=
+  s.foldl addMink addMink_right_comm ⊥ -- OK for `CanonicallyOrderedAddCommMonoid`
 
-def Multiset.sumMink (s : Multiset (Order.PFilter C)) : Order.PFilter C :=
-  s.foldl addMink sorry 0
-
-instance : HSMul ℕ (Order.PFilter C) (Order.PFilter C) where
+instance : HSMul ℕ (UpperSet C) (UpperSet C) where
   hSMul := fun
-  | .zero => 0
+  | .zero => ⊥ -- this is `Set.univ` but we should rather use `{ x | 0 ≤ x }`
   | .succ n => fun x => addMink x sorry -- add `(n • x)` from induction
 
 
@@ -42,7 +44,7 @@ structure FilterValuedCsp.Term (Γ : FilterValuedCsp D C) (ι : Type*) where
   /-- Arity of the function -/
   n : ℕ
   /-- Which cost function is instantiated -/
-  f : (Fin n → D) → Order.PFilter C
+  f : (Fin n → D) → UpperSet C
   /-- The cost function comes from the template -/
   inΓ : ⟨n, f⟩ ∈ Γ
   /-- Which variables are plugged as arguments to the cost function -/
@@ -50,7 +52,7 @@ structure FilterValuedCsp.Term (Γ : FilterValuedCsp D C) (ι : Type*) where
 
 /-- Evaluation of a `Γ` term `t` for given solution `x`. -/
 def FilterValuedCsp.Term.evalSolution {Γ : FilterValuedCsp D C} {ι : Type*}
-    (t : Γ.Term ι) (x : ι → D) : Order.PFilter C :=
+    (t : Γ.Term ι) (x : ι → D) : UpperSet C :=
   t.f (x ∘ t.app)
 
 /-- A filter-valued CSP instance over the template `Γ` with variables indexed by `ι`. -/
@@ -59,7 +61,7 @@ abbrev FilterValuedCsp.Instance (Γ : FilterValuedCsp D C) (ι : Type*) : Type _
 
 /-- Evaluation of a `Γ` instance `I` for given solution `x`. -/
 def FilterValuedCsp.Instance.evalSolution {Γ : FilterValuedCsp D C} {ι : Type*}
-    (I : Γ.Instance ι) (x : ι → D) : Order.PFilter C :=
+    (I : Γ.Instance ι) (x : ι → D) : UpperSet C :=
   (I.map (fun t : Γ.Term ι => t.evalSolution x)).sumMink
 
 /-- Condition for `x` being an optimum solution (min) to given `Γ` instance `I`. -/
@@ -70,22 +72,23 @@ def FilterValuedCsp.Instance.IsOptimumSolution {Γ : FilterValuedCsp D C} {ι : 
 
 /-- Partial evaluation of a `Γ` instance `I` for given partial solution `x` waiting for rest. -/
 def FilterValuedCsp.Instance.evalPartial {Γ : FilterValuedCsp D C} {ι μ : Type*}
-    (I : Γ.Instance (ι ⊕ μ)) (x : ι → D) : (μ → D) → Order.PFilter C :=
+    (I : Γ.Instance (ι ⊕ μ)) (x : ι → D) : (μ → D) → UpperSet C :=
   fun r => I.evalSolution (Sum.elim x r)
 
 /-- Evaluation of a `Γ` instance `I` for given partial solution `x`, union over the rest. -/
 def FilterValuedCsp.Instance.evalMinimize {Γ : FilterValuedCsp D C} {ι μ : Type*}
-    (I : Γ.Instance (ι ⊕ μ)) (x : ι → D) : Order.PFilter C :=
-  sorry -- Union (I.evalPartial x)
+    (I : Γ.Instance (ι ⊕ μ)) (x : ι → D) : UpperSet C :=
+  sorry -- ⟨Set.iUnion (I.evalPartial x), sorry⟩
 
 /-- A new VCSP template made of all functions expressible by `Γ`. -/
 def FilterValuedCsp.expressivePower (Γ : FilterValuedCsp D C) : FilterValuedCsp D C :=
   { ⟨n, I.evalMinimize⟩ | (n : ℕ) (m : ℕ) (I : Γ.Instance (Fin n ⊕ Fin m)) }
 
+
 variable {m : ℕ}
 
 def Function.AdmitsFilterFractional {n : ℕ}
-    (f : (Fin n → D) → Order.PFilter C) (ω : FractionalOperation D m) : Prop :=
+    (f : (Fin n → D) → UpperSet C) (ω : FractionalOperation D m) : Prop :=
   ∀ x : (Fin m → (Fin n → D)),
     ω.size • (Finset.univ.val.map (fun i => f (x i))).sumMink ≤ m • ((ω.tt x).map f).sumMink
 
@@ -105,7 +108,7 @@ def FilterValuedCsp.allFractionalPolymorphisms (Γ : FilterValuedCsp D C) :
 
 def Set.largestFilterValuedCsp (S : Set (Σ (m : ℕ), FractionalOperation D m)) :
     FilterValuedCsp D C :=
-  { ⟨n, f⟩ | (n : ℕ) (f : (Fin n → D) → Order.PFilter C) (_ : ∀ ω ∈ S, f.AdmitsFilterFractional ω.snd) }
+  { ⟨n, f⟩ | (n : ℕ) (f : (Fin n → D) → UpperSet C) (_ : ∀ ω ∈ S, f.AdmitsFilterFractional ω.snd) }
 
 def FilterValuedCsp.closure (Γ : FilterValuedCsp D C) : FilterValuedCsp D C :=
   Γ.allFractionalPolymorphisms.largestFilterValuedCsp
