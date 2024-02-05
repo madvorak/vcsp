fa99c14a26daaefe10bb222afb45fc934563a393
madvorak <martin.dvorak@matfyz.cz>
2024-02-05 15:19:50 +0100

    basic LP relaxation simplified

--- VCSP/LinearRelaxation.lean
+++ VCSP/LinearRelaxation.lean
@@ -11,7 +11,7 @@ def ValuedCSP.Instance.LPcons {Γ : ValuedCSP D ℚ} {ι : Type} [Fintype ι] (I
   (Σ t : {_t : Γ.Term ι // _t ∈ I}, (Fin t.val.n × D)) ⊕ ι ⊕ LPvars I
 
 /-
-For all `⟨t, j, a⟩` in `(Π t ∈ I, Fin t.n × D)`, the sum of all |D| ^ (t.n - 1)
+For all `⟨t, j, a⟩` in `(Σ t ∈ I, Fin t.n × D)`, the sum of all |D| ^ (t.n - 1)
   `Sum.inl ⟨t, (x : Fin t.n → D | x j = a)⟩` must be equal to `Sum.inr (t.app j, a)`.
 For all `i` in `ι`, the sum of all |D| `Sum.inr (i, _)` must be `1`.
 Each `v` in `LPvars I` must be between `0` and `1`.
@@ -19,7 +19,7 @@ Each `v` in `LPvars I` must be between `0` and `1`.
 Ideally (--> tight relaxation)...
 For each `i` in `ι`, there is exactly one `a` in `D` where
   `Sum.inr (i, a)` is `1` and all other `Sum.inr (i, _)` are `0`.
-For all `⟨t, j⟩` in `(Π t ∈ I, Fin t.n)`:
+For all `⟨t, j⟩` in `(Σ t ∈ I, Fin t.n)`:
   · If `Sum.inr (t.app j, a)` is `0` then all `Sum.inl ⟨t, (x : Fin t.n → D | x j = a)⟩` are `0`.
   · If `Sum.inr (t.app j, a)` is `1` then there is exactly one `x : Fin t.n → D | x j = a` where
     `Sum.inl ⟨t, x⟩` is `1` and all other `Sum.inl ⟨t, (x : Fin t.n → D | x j = a)⟩` are `0`.
@@ -29,12 +29,13 @@ def ValuedCSP.Instance.LPrelax {Γ : ValuedCSP D ℚ} {ι : Type} [Fintype ι] [
     (I : Γ.Instance ι)
     [Fintype I.LPcons] [Fintype I.LPvars] : -- TODO these two must be inferred automatically!!
     StandardLP I.LPcons I.LPvars ℚ :=
+  have _ : DecidableEq (I.LPvars) := instDecidableEqSum
   StandardLP.mk (
     fun
     | .inl ⟨cₜ, cᵢ, cₐ⟩ => fun
       | .inl ⟨t, x⟩ =>
-        if ht : cₜ = t then
-          if x (cast (congr_arg (Fin ∘ Term.n ∘ Subtype.val) ht) cᵢ) = cₐ
+        if ht : cₜ.val = t.val then
+          if x (cast (congr_arg (Fin ∘ Term.n) ht) cᵢ) = cₐ
           then 1
           else 0
         else 0
@@ -42,12 +43,8 @@ def ValuedCSP.Instance.LPrelax {Γ : ValuedCSP D ℚ} {ι : Type} [Fintype ι] [
     | .inr (.inl cᵢ) => fun
       | .inl _ => 0
       | .inr ⟨i, _⟩ => if cᵢ = i then 1 else 0
-    | .inr (.inr (.inl c)) => fun
-      | .inl v => if c = v then 1 else 0
-      | .inr _ => 0
-    | .inr (.inr (.inr ⟨cᵢ, cₐ⟩)) => fun
-      | .inl _ => 0
-      | .inr ⟨i, a⟩ => if cᵢ = i ∧ cₐ = a then 1 else 0
+    | .inr (.inr cᵥ) => fun
+        v => if cᵥ = v then 1 else 0
   ) (
     fun
     | .inl _ => 0
