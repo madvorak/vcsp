1e5b56805e029d1ea31eaaf5e3d26cb4fa1a23a3
madvorak <martin.dvorak@matfyz.cz>
2024-02-13 20:20:21 +0100

    refactoring to Matrix.fromBlocks

--- VCSP/LinearRelaxation.lean
+++ VCSP/LinearRelaxation.lean
@@ -21,14 +21,14 @@ def ValuedCSP.Instance.LPrelaxation (I : Γ.Instance ι) :
   BothieLP.mk
     1 -- the identity matrix
     1 -- the all ones vector
-    (Matrix.fromRows
-      (Matrix.fromColumns
-        (Matrix.of fun ⟨⟨cₜ, _⟩, cᵢ, cₐ⟩ => fun ⟨⟨t, _⟩, x⟩ =>
-          if ht : cₜ.n = t.n
-          then if x (Fin.cast ht cᵢ) = cₐ then 1 else 0
-          else 0)
-        (Matrix.of fun ⟨⟨cₜ, _⟩, cᵢ, cₐ⟩ => fun ⟨i, a⟩ => if cₜ.app cᵢ = i ∧ cₐ = a then -1 else 0))
-      (Matrix.fromColumns 0 (Matrix.of fun cᵢ : ι => fun ⟨i, _⟩ => if cᵢ = i then 1 else 0)))
+    (Matrix.fromBlocks
+      (Matrix.of fun ⟨⟨cₜ, _⟩, cᵢ, cₐ⟩ => fun ⟨⟨t, _⟩, x⟩ =>
+        if ht : cₜ.n = t.n
+        then if x (Fin.cast ht cᵢ) = cₐ then 1 else 0
+        else 0)
+      (Matrix.of fun ⟨⟨cₜ, _⟩, cᵢ, cₐ⟩ => fun ⟨i, a⟩ => if cₜ.app cᵢ = i ∧ cₐ = a then -1 else 0)
+      0
+      (Matrix.of fun cᵢ : ι => fun ⟨i, _⟩ => if cᵢ = i then 1 else 0))
     (Sum.elim
       (fun _ : (Σ t : I, (Fin t.fst.n × D)) => 0)
       (fun _ : ι => 1))
@@ -43,19 +43,6 @@ lemma sumType_zeroFun_dotProduct {α β : Type} [Fintype α] [Fintype β]
     (u v : α → ℚ) (v' : β → ℚ) :
     (Sum.elim u 0) ⬝ᵥ (Sum.elim v v') = u ⬝ᵥ v := by
   rw [Matrix.sum_elim_dotProduct_sum_elim, Matrix.zero_dotProduct, add_zero]
-
-lemma zeroMat_fromColumns_mulVec {α β γ : Type} [Fintype α] [Fintype β] [Fintype γ]
-    (A₂ : Matrix α γ ℚ) (v₁ : β → ℚ) (v₂ : γ → ℚ) :
-    Matrix.fromColumns 0 A₂ *ᵥ Sum.elim v₁ v₂ = A₂ *ᵥ v₂ := by
-  rw [Matrix.fromColumns_mulVec_sum_elim, Matrix.zero_mulVec, zero_add]
-
-private lemma zeroMat_fromColumns_mulVec_sumElim_index_le_one {α β γ : Type}
-    (_ : Fintype α) (_ : Fintype β) (_ : Fintype γ)
-    {A₂ : Matrix α γ ℚ} {v₁ : β → ℚ} {v₂ : γ → ℚ} {a : α}
-    (todo : (A₂ *ᵥ v₂) a ≤ 1) :
-    (Matrix.fromColumns 0 A₂ *ᵥ Sum.elim v₁ v₂) a ≤ 1 := by
-  rw [zeroMat_fromColumns_mulVec]
-  exact todo
 
 @[pp_dot]
 abbrev ValuedCSP.Instance.solutionVCSPtoLP (I : Γ.Instance ι) (x : ι → D) :
