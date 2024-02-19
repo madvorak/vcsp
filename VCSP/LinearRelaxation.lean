cab95b8b011544d6c98a99c23d0b05ee05f08a3b
madvorak <martin.dvorak@matfyz.cz>
2024-02-19 19:21:36 +0100

    refactoring

--- VCSP/LinearRelaxation.lean
+++ VCSP/LinearRelaxation.lean
@@ -6,7 +6,7 @@ open scoped Matrix
 
 
 -- TODO probably upstream (otherwise rename arguments)
-lemma Matrix.fromBlocks_mulVec_sum_elim {l m n o R : Type*} [Semiring R]
+lemma Matrix.fromBlocks_mulVec_sumType {l m n o R : Type*} [Semiring R]
     [Fintype l] [Fintype m] [Fintype n] [Fintype o]
     (A : Matrix n l R) (B : Matrix n m R) (C : Matrix o l R) (D : Matrix o m R)
     (u : l → R) (v : m → R) :
@@ -19,11 +19,11 @@ lemma sumType_zeroFun_dotProduct {α β R : Type} [Fintype α] [Fintype β] [Sem
     Sum.elim u 0 ⬝ᵥ Sum.elim v v' = u ⬝ᵥ v := by
   rw [Matrix.sum_elim_dotProduct_sum_elim, Matrix.zero_dotProduct, add_zero]
 
-lemma neg_finset_sum_univ {α R : Type} [Fintype α] [Ring R] (f : α → R) :
-    - (Finset.sum Finset.univ f) = Finset.sum Finset.univ (- f) := by
+lemma neg_finset_univ_sum {α R : Type} [Fintype α] [Ring R] (f : α → R) :
+    - Finset.univ.sum f = Finset.univ.sum (-f) := by
   simp only [Pi.neg_apply, Finset.sum_neg_distrib]
 
-lemma neg_indicator_neg {α R : Type} [Fintype α] [Ring R] (P : α → Prop) [DecidablePred P] :
+lemma indicator_of_neg {α R : Type} [Fintype α] [Ring R] (P : α → Prop) [DecidablePred P] :
     - (fun x => if P x then -1 else (0 : R)) = (fun x => if P x then 1 else 0) := by
   aesop
 
@@ -76,8 +76,15 @@ lemma ValuedCSP.Instance.solutionVCSPtoLP_nneg (I : Γ.Instance ι) (x : ι → 
 
 lemma ValuedCSP.Instance.solutionVCSPtoLP_cost (I : Γ.Instance ι) (x : ι → D) :
     I.LPrelaxation.c ⬝ᵥ I.solutionVCSPtoLP x = I.evalSolution x := by
-  simp [LPrelaxation, solutionVCSPtoLP, evalSolution, Matrix.dotProduct] -- TODO refactor
+  simp [ValuedCSP.Instance.LPrelaxation, ValuedCSP.Instance.solutionVCSPtoLP,
+        ValuedCSP.Instance.evalSolution, ValuedCSP.Term.evalSolution, Matrix.dotProduct]
+  show
+    Finset.univ.sum
+      (fun (⟨⟨t, _⟩, v⟩ : Σ t : I, (Fin t.fst.n → D)) =>
+        t.f v * if (∀ i : Fin t.n, v i = x (t.app i)) then 1 else 0) =
+    I.summap (fun t => t.f (x ∘ t.app))
   sorry
+
 
 lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_left_of_hit (I : Γ.Instance ι)
     {cₜ : Σ t : Γ.Term ι, Fin (I.count t)} {cₙ : Fin cₜ.fst.n} {cₐ : D} {x : ι → D}
@@ -93,19 +100,20 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_left_of_hit (I : Γ.I
     1 := by
   rw [Sum.elim_comp_inl, Matrix.dotProduct]
   show
-    Finset.sum Finset.univ (fun (i : Σ t : I, (Fin t.fst.n → D)) =>
-      (match i with
-        | Sigma.mk t y =>
+    -- DO NOT refactor to `fun (⟨t, y⟩ : Σ t : I, (Fin t.fst.n → D))` as it would hinder aesop
+    Finset.univ.sum (fun (tᵥ : Σ t : I, (Fin t.fst.n → D)) =>
+      (match tᵥ with
+        | ⟨t, v⟩ =>
           if ht : cₜ = t
           then
-            if y (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
+            if v (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
             then 1
             else 0
           else 0
         ) *
-      (match i with
-        | Sigma.mk t y =>
-          if (∀ i : Fin t.fst.n, y i = x (t.fst.app i))
+      (match tᵥ with
+        | ⟨t, v⟩ =>
+          if (∀ i : Fin t.fst.n, v i = x (t.fst.app i))
           then 1
           else 0
         )
@@ -113,15 +121,15 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_left_of_hit (I : Γ.I
     (1 : C)
   simp_rw [mul_ite, mul_one, mul_zero]
   convert_to
-    Finset.sum Finset.univ (fun (i : Σ t : I, (Fin t.fst.n → D)) =>
-      (match i with
-        | Sigma.mk t y =>
+    Finset.univ.sum (fun (tᵥ : Σ t : I, (Fin t.fst.n → D)) =>
+      (match tᵥ with
+        | ⟨t, v⟩ =>
           if (
-            if (∀ i : Fin t.fst.n, y i = x (t.fst.app i))
+            if (∀ i : Fin t.fst.n, v i = x (t.fst.app i))
             then
               if ht : cₜ = t
               then
-                if y (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
+                if v (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
                 then True
                 else False
               else False
@@ -153,19 +161,19 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_left_of_miss (I : Γ.
     0 := by
   rw [Sum.elim_comp_inl, Matrix.dotProduct]
   show
-    Finset.sum Finset.univ (fun (i : Σ t : I, (Fin t.fst.n → D)) =>
-      (match i with
-        | Sigma.mk t y =>
+    Finset.univ.sum (fun (tᵥ : Σ t : I, (Fin t.fst.n → D)) =>
+      (match tᵥ with
+        | ⟨t, v⟩ =>
           if ht : cₜ = t
           then
-            if y (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
+            if v (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
             then 1
             else 0
           else 0
         ) *
-      (match i with
-        | Sigma.mk t y =>
-          if (∀ i : Fin t.fst.n, y i = x (t.fst.app i))
+      (match tᵥ with
+        | ⟨t, v⟩ =>
+          if (∀ i : Fin t.fst.n, v i = x (t.fst.app i))
           then 1
           else 0
         )
@@ -173,15 +181,15 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_left_of_miss (I : Γ.
     (0 : C)
   simp_rw [mul_ite, mul_one, mul_zero]
   convert_to
-    Finset.sum Finset.univ (fun (i : Σ t : I, (Fin t.fst.n → D)) =>
-      (match i with
-        | Sigma.mk t y =>
+    Finset.univ.sum (fun (tᵥ : Σ t : I, (Fin t.fst.n → D)) =>
+      (match tᵥ with
+        | ⟨t, v⟩ =>
           if (
-            if (∀ i : Fin t.fst.n, y i = x (t.fst.app i))
+            if (∀ i : Fin t.fst.n, v i = x (t.fst.app i))
             then
               if ht : cₜ = t
               then
-                if y (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
+                if v (Fin.cast (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
                 then True
                 else False
               else False
@@ -202,7 +210,7 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_right_of_hit (I : Γ.
     (fun ⟨i, a⟩ => if cₜ.fst.app cₙ = i ∧ cₐ = a then -1 else 0) ⬝ᵥ (I.solutionVCSPtoLP x ∘ Sum.inr) = -1 := by
   rw [Sum.elim_comp_inr, Matrix.dotProduct]
   simp_rw [mul_ite, mul_one, mul_zero, ←ite_and]
-  rw [←neg_eq_iff_eq_neg, neg_finset_sum_univ, neg_indicator_neg]
+  rw [←neg_eq_iff_eq_neg, neg_finset_univ_sum, indicator_of_neg]
   rw [Finset.sum_boole, Nat.cast_eq_one, Finset.card_eq_one]
   use (cₜ.fst.app cₙ, cₐ)
   aesop
@@ -213,7 +221,7 @@ lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_top_right_of_miss (I : Γ
     (fun ⟨i, a⟩ => if cₜ.fst.app cₙ = i ∧ cₐ = a then -1 else 0) ⬝ᵥ (I.solutionVCSPtoLP x ∘ Sum.inr) = -0 := by
   rw [Sum.elim_comp_inr, Matrix.dotProduct]
   simp_rw [mul_ite, mul_one, mul_zero, ←ite_and]
-  rw [←neg_eq_iff_eq_neg, neg_finset_sum_univ, neg_indicator_neg, Finset.sum_boole]
+  rw [←neg_eq_iff_eq_neg, neg_finset_univ_sum, indicator_of_neg, Finset.sum_boole]
   aesop
 
 lemma ValuedCSP.Instance.LPrelaxation_solutionVCSPtoLP_bottom_right (I : Γ.Instance ι)
@@ -232,7 +240,7 @@ theorem ValuedCSP.Instance.LPrelaxation_Reaches (I : Γ.Instance ι) (x : ι →
   · simp only [CanonicalLP.IsSolution, ValuedCSP.Instance.LPrelaxation]
     constructor
     · ext j
-      rw [Matrix.fromBlocks_mulVec_sum_elim]
+      rw [Matrix.fromBlocks_mulVec_sumType]
       rw [Matrix.zero_mulVec, zero_add]
       cases j with
       | inl c =>
