23ffdcabfcc52d31ac8af3d2a53a757730f36300
madvorak <martin.dvorak@matfyz.cz>
2024-02-19 11:45:35 +0100

    decomposition of ValuedCSP.Instance.LPrelaxation_Reaches

--- VCSP/LinearRelaxation.lean
+++ VCSP/LinearRelaxation.lean
@@ -2,6 +2,23 @@ import VCSP.Hardness
 import VCSP.LinearProgramming
 import Mathlib.Data.Multiset.Fintype
 
+open scoped Matrix
+
+
+-- TODO probably upstream (otherwise rename arguments)
+lemma Matrix.fromBlocks_mulVec_sum_elim {l m n o R : Type*} [Semiring R]
+    [Fintype l] [Fintype m] [Fintype n] [Fintype o]
+    (A : Matrix n l R) (B : Matrix n m R) (C : Matrix o l R) (D : Matrix o m R)
+    (u : l → R) (v : m → R) :
+    Matrix.fromBlocks A B C D *ᵥ Sum.elim u v = Sum.elim (A *ᵥ u + B *ᵥ v) (C *ᵥ u + D *ᵥ v) := by
+  rw [← Matrix.fromRows_fromColumn_eq_fromBlocks, Matrix.fromRows_mulVec,
+    Matrix.fromColumns_mulVec_sum_elim, Matrix.fromColumns_mulVec_sum_elim]
+
+lemma sumType_zeroFun_dotProduct {α β R : Type} [Fintype α] [Fintype β] [Semiring R]
+    (u v : α → R) (v' : β → R) :
+    Sum.elim u 0 ⬝ᵥ Sum.elim v v' = u ⬝ᵥ v := by
+  rw [Matrix.sum_elim_dotProduct_sum_elim, Matrix.zero_dotProduct, add_zero]
+
 
 variable
   {D : Type} [Nonempty D] [Fintype D] [DecidableEq D]
@@ -37,14 +54,6 @@ def ValuedCSP.Instance.LPrelaxation (I : Γ.Instance ι) :
       (fun ⟨⟨t, _⟩, x⟩ => t.f x)
       (fun _ => 0))
 
-
-open scoped Matrix
-
-lemma sumType_zeroFun_dotProduct {α β : Type} [Fintype α] [Fintype β]
-    (u v : α → C) (v' : β → C) :
-    Sum.elim u 0 ⬝ᵥ Sum.elim v v' = u ⬝ᵥ v := by
-  rw [Matrix.sum_elim_dotProduct_sum_elim, Matrix.zero_dotProduct, add_zero]
-
 @[pp_dot]
 abbrev ValuedCSP.Instance.solutionVCSPtoLP (I : Γ.Instance ι) (x : ι → D) :
     ((Σ t : I, (Fin t.fst.n → D)) ⊕ (ι × D)) → C :=
@@ -52,7 +61,40 @@ abbrev ValuedCSP.Instance.solutionVCSPtoLP (I : Γ.Instance ι) (x : ι → D) :
     (fun ⟨⟨t, _⟩, (v : (Fin t.n → D))⟩ => if ∀ i : Fin t.n, v i = x (t.app i) then 1 else 0)
     (fun ⟨i, d⟩ => if x i = d then 1 else 0)
 
-lemma ValuedCSP.Instance.LPrelaxation_iota (I : Γ.Instance ι) (cᵢ : ι) (x : ι → D) :
+lemma ValuedCSP.Instance.solutionVCSPtoLP_nneg (I : Γ.Instance ι) (x : ι → D) :
+    0 ≤ I.solutionVCSPtoLP x := by
+  sorry
+
+lemma ValuedCSP.Instance.solutionVCSPtoLP_cost (I : Γ.Instance ι) (x : ι → D) :
+    I.LPrelaxation.c ⬝ᵥ I.solutionVCSPtoLP x = I.evalSolution x := by
+  sorry
+
+lemma ValuedCSP.Instance.LPrelaxation_top_left (I : Γ.Instance ι)
+    (cₜ : Σ t : Γ.Term ι, Fin (I.count t)) (cₙ : Fin cₜ.fst.n) (cₐ : D) (x : ι → D) :
+    (fun ⟨t, x⟩ =>
+      if ht : cₜ = t
+      then
+        if x (@Fin.cast cₜ.fst.n t.fst.n (congr_arg (Term.n ∘ Sigma.fst) ht) cₙ) = cₐ
+        then 1
+        else 0
+      else 0
+      ) ⬝ᵥ (I.solutionVCSPtoLP x ∘ Sum.inl) =
+    1 := by
+  unfold ValuedCSP.Instance.solutionVCSPtoLP
+  simp only [Sum.elim_comp_inl]
+  rw [Matrix.dotProduct]
+  sorry
+
+lemma ValuedCSP.Instance.LPrelaxation_top_right (I : Γ.Instance ι)
+    (cₜ : Σ t : Γ.Term ι, Fin (I.count t)) (cₙ : Fin cₜ.fst.n) (cₐ : D) (x : ι → D) :
+    (fun ⟨i, a⟩ => if cₜ.fst.app cₙ = i ∧ cₐ = a then -1 else 0) ⬝ᵥ (I.solutionVCSPtoLP x ∘ Sum.inr) = -1 := by
+  unfold ValuedCSP.Instance.solutionVCSPtoLP
+  simp only [Sum.elim_comp_inr]
+  rw [Matrix.dotProduct]
+  simp_rw [mul_ite, mul_one, mul_zero, ←ite_and]
+  sorry
+
+lemma ValuedCSP.Instance.LPrelaxation_bottom_right (I : Γ.Instance ι) (cᵢ : ι) (x : ι → D) :
     (fun ⟨i, _⟩ => if cᵢ = i then 1 else 0) ⬝ᵥ (I.solutionVCSPtoLP x ∘ Sum.inr) = 1 := by
   unfold ValuedCSP.Instance.solutionVCSPtoLP
   simp only [Sum.elim_comp_inr]
@@ -61,15 +103,6 @@ lemma ValuedCSP.Instance.LPrelaxation_iota (I : Γ.Instance ι) (cᵢ : ι) (x :
   rw [Finset.sum_boole, Nat.cast_eq_one, Finset.card_eq_one]
   use (cᵢ, x cᵢ)
   aesop
-
--- TODO probably upstream (otherwise rename arguments)
-lemma Matrix.fromBlocks_mulVec_sum_elim {l m n o R : Type*} [Semiring R]
-    [Fintype l] [Fintype m] [Fintype n] [Fintype o]
-    (A : Matrix n l R) (B : Matrix n m R) (C : Matrix o l R) (D : Matrix o m R)
-    (u : l → R) (v : m → R) :
-    Matrix.fromBlocks A B C D *ᵥ Sum.elim u v = Sum.elim (A *ᵥ u + B *ᵥ v) (C *ᵥ u + D *ᵥ v) := by
-  rw [← Matrix.fromRows_fromColumn_eq_fromBlocks, Matrix.fromRows_mulVec,
-    Matrix.fromColumns_mulVec_sum_elim, Matrix.fromColumns_mulVec_sum_elim]
 
 theorem ValuedCSP.Instance.LPrelaxation_Reaches (I : Γ.Instance ι) (x : ι → D) :
     I.LPrelaxation.Reaches (I.evalSolution x) := by
@@ -83,10 +116,13 @@ theorem ValuedCSP.Instance.LPrelaxation_Reaches (I : Γ.Instance ι) (x : ι →
       cases j with
       | inl c =>
         obtain ⟨cₜ, cₙ, cₐ⟩ := c
-        rw [Sum.elim_inl, Sum.elim_inl]
-        sorry
+        rw [Sum.elim_inl, Sum.elim_inl, Pi.add_apply]
+        convert_to (1 : C) + (-1) = 0 using 2
+        · exact I.LPrelaxation_top_left  cₜ cₙ cₐ x
+        · exact I.LPrelaxation_top_right cₜ cₙ cₐ x
+        exact add_neg_self 1
       | inr cᵢ =>
         rw [Sum.elim_inr, Sum.elim_inr]
-        exact I.LPrelaxation_iota cᵢ x
-    · sorry
-  · sorry
+        exact I.LPrelaxation_bottom_right cᵢ x
+    · exact I.solutionVCSPtoLP_nneg x
+  · exact I.solutionVCSPtoLP_cost x
