ecc4dda1868d49affcf064878c69b9030927c672
madvorak <martin.dvorak@matfyz.cz>
2024-02-28 12:34:57 +0100

    wip

--- VCSP/LinearRelaxationAndSFP.lean
+++ VCSP/LinearRelaxationAndSFP.lean
@@ -5,16 +5,58 @@ variable
   {ι : Type} [Nonempty ι] [Fintype ι] [DecidableEq ι]
   {Γ : ValuedCSP D ℚ} [DecidableEq (Γ.Term ι)]
 
+def aux (κ : D → ℕ) : ∃ m : ℕ, ∃ v : Fin m → D, ∀ a : D, 0 < κ a ↔ ∃ i : Fin m, v i = a := by
+  let l : List D := List.join (Finset.univ.val.map (fun d : D => List.replicate (κ d) d)).toList
+  use l.length
+  use l.get
+  intro a
+  constructor <;> intro hyp
+  · apply List.get_of_mem
+    rw [List.mem_join]
+    simp only [Multiset.mem_toList, Multiset.mem_map, Finset.mem_val, Finset.mem_univ, true_and]
+    rw [exists_exists_eq_and]
+    use a
+    rw [List.mem_replicate]
+    constructor
+    · rwa [Nat.pos_iff_ne_zero] at hyp
+    · rfl
+  · rw [←List.mem_iff_get, List.mem_join] at hyp
+    simp only [Multiset.mem_toList, Multiset.mem_map, Finset.mem_val, Finset.mem_univ, true_and] at hyp
+    rw [exists_exists_eq_and] at hyp
+    obtain ⟨a', ha'⟩ := hyp
+    rw [List.mem_replicate] at ha'
+    obtain ⟨ha, rfl⟩ := ha'
+    exact Nat.pos_of_ne_zero ha
+
 example (δ : D → ℚ) (non_neg : 0 ≤ δ) (sum_one : Finset.univ.sum δ = 1) :
     ∃ m : ℕ, ∃ v : Fin m → D, ∀ a : D, 0 < δ a ↔ ∃ i : Fin m, v i = a := by
   let w : D → ℕ := fun a : D =>
     Finset.univ.prod (fun b : D => if a = b then (δ b).num.toNat else (δ b).den)
-  use Finset.univ.sum w
-  --let l : List D := List.join (Finset.univ.val.map (fun d : D => List.replicate (w d) d)).toList
-  --use l.length
-  sorry
+  obtain ⟨m, v, rest⟩ := aux w
+  use m
+  use v
+  intro a
+  rw [← rest a]
+  constructor <;> intro hyp
+  · sorry
+  · apply lt_of_le_of_ne
+    · exact non_neg a
+    symm
+    apply ne_of_gt at hyp
+    rw [Finset.prod_ne_zero_iff] at hyp
+    convert hyp a (Finset.mem_univ a)
+    simp only [↓reduceIte, Int.toNat_eq_zero]
+    have triv : (δ a).num ≤ 0 ↔ (δ a).num = 0
+    · constructor
+      · intro hle
+        apply eq_of_le_of_not_lt hle
+        push_neg
+        exact Rat.num_nonneg_iff_zero_le.mpr (non_neg a)
+      · apply Eq.le
+    rw [triv]
+    apply Rat.zero_iff_num_zero
 
--- TODO change to perhaps `∃ m : ℕ, ∃ v : Fin m → D, ` (properties of `v` wrt `δ`)
+-- TODO change to perhaps `∃ m : ℕ, ∃ v : Fin m → ι → D, ` (properties of `v` wrt `δ`)
 def convertDistribution {δ : ι → D → ℚ} (nonneg : 0 ≤ δ) : Σ m : ℕ, Fin m → ι → D := sorry
   -- We have a discrete probability distributions over rationals.
   -- How do we get them all to a common denominator?
