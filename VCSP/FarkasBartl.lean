7cfea65faa98569fb624a862ca2812661abbbbcf
madvorak <martin.dvorak@matfyz.cz>
2024-05-13 18:16:39 +0200

    unused files removed

--- VCSP/FarkasBartl.lean
+++ VCSP/FarkasBartl.lean
@@ -11,7 +11,7 @@ variable {I : Type*} [Fintype I] -- typically `Fin m`
 /- The paper by Bartl is actually more general, in particular allowing "skew fields" (`DivisionRing`),
    but Mathlib does not seem to have a definition `LinearOrderedDivisionRing`,
    so we work with `LinearOrderedField` for now. -/
-theorem generalFarkasBartl {F V W : Type*} [LinearOrderedField F] -- typically `V` = `F` and `W` = `F^n`
+theorem generalizedFarkasBartl {F V W : Type*} [LinearOrderedField F] -- typically `V` = `F` and `W` = `F^n`
     [LinearOrderedAddCommGroup V] [Module F V] [CompatiblyOrdered F V] [AddCommGroup W] [Module F W]
     (A : I → W →ₗ[F] F) (b : W →ₗ[F] V) :
     (∀ x : W, (∀ i : I, A i x ≤ 0) → b x ≤ 0) ↔ (∃ U : I → V, (∀ i : I, 0 ≤ U i) ∧
@@ -23,4 +23,4 @@ open Matrix
 
 theorem equalityFarkas (A : Matrix I J ℚ) (b : I → ℚ) :
     (∃ x : J → ℚ, 0 ≤ x ∧ A *ᵥ x = b) ≠ (∃ y : I → ℚ, -Aᵀ *ᵥ y ≤ 0 ∧ b ⬝ᵥ y < 0) := by
-  sorry -- TODO prove it follows from `generalFarkasBartl`
+  sorry -- TODO prove it follows from `generalizedFarkasBartl`
