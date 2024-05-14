9830bfe8331f3b75ce9a2f059c234df2b627f4f6
madvorak <martin.dvorak@matfyz.cz>
2024-05-14 11:25:35 +0200

    equalityFarkas is a corollary or generalizedFarkasBartl

--- VCSP/FarkasBartl.lean
+++ VCSP/FarkasBartl.lean
@@ -1,4 +1,5 @@
 import Mathlib.LinearAlgebra.Matrix.DotProduct
+import Mathlib.LinearAlgebra.Matrix.ToLin
 import VCSP.Basic
 
 
@@ -14,17 +15,62 @@ variable {I : Type*} [Fintype I] -- typically `Fin m`
    so we work with `LinearOrderedField` for now. -/
 theorem generalizedFarkasBartl {F V W : Type*} [LinearOrderedField F] -- typically `V` = `F` and `W` = `F^n`
     [LinearOrderedAddCommGroup V] [Module F V] [CompatiblyOrdered F V] [AddCommGroup W] [Module F W]
-    (A : I → W →ₗ[F] F) (b : W →ₗ[F] V) :
-    (∀ x : W, (A · x) ≤ 0 → b x ≤ 0) ↔ (∃ U : I → V, 0 ≤ U ∧
-        ∀ w : W, b w = Finset.univ.sum (fun i : I => A i w • U i)) := by
+    (A : W →ₗ[F] I → F) (b : W →ₗ[F] V) :
+    (∀ x : W, A x ≤ 0 → b x ≤ 0) ↔ (∃ U : I → V, 0 ≤ U ∧
+        ∀ w : W, b w = Finset.univ.sum (fun i : I => A w i • U i)) := by
   sorry
 
-variable {J : Type*} [Fintype J]
+
+section corollaries
+
+lemma of_iff {P Q : Prop} (hpq : P ↔ Q) : (¬P) ≠ Q := by
+  tauto
+
 open Matrix
+variable {J : Type*} [Fintype J]
+
+lemma Matrix.neg_mulVec_neg {R : Type*} [Ring R] (v : J → R) (A : Matrix I J R) : (-A) *ᵥ (-v) = A *ᵥ v := by
+  rw [Matrix.mulVec_neg, Matrix.neg_mulVec, neg_neg]
+
+macro "finishit" : tactic => `(tactic|
+  unfold Matrix.mulVec Matrix.vecMul Matrix.dotProduct <;>
+  simp_rw [Finset.sum_mul] <;> rw [Finset.sum_comm] <;>
+  congr <;> ext <;> congr <;> ext <;> ring)
 
 theorem equalityFarkas (A : Matrix I J ℚ) (b : I → ℚ) :
     (∃ x : J → ℚ, 0 ≤ x ∧ A *ᵥ x = b) ≠ (∃ y : I → ℚ, -Aᵀ *ᵥ y ≤ 0 ∧ b ⬝ᵥ y < 0) := by
-  let A' : J → (I → ℚ) →ₗ[ℚ] ℚ := fun i : J => ⟨⟨fun z : I → ℚ => (Aᵀ *ᵥ z) i, fun x y => by sorry⟩, by sorry⟩
-  let b' : (I → ℚ) →ₗ[ℚ] ℚ := sorry
-  have := generalizedFarkasBartl A' b'
+  have gener :=
+    of_iff
+      (generalizedFarkasBartl Aᵀ.mulVecLin (⟨⟨(b ⬝ᵥ ·), Matrix.dotProduct_add b⟩, (Matrix.dotProduct_smul · b)⟩))
+  push_neg at gener
+  convert gener.symm using 1 <;> clear gener <;> constructor
+  · intro ⟨x, hx, hAxb⟩
+    refine ⟨x, hx, ?_⟩
+    intro
+    simp
+    rw [←hAxb]
+    finishit
+  · intro ⟨U, hU, hAU⟩
+    refine ⟨U, hU, ?_⟩
+    simp at hAU
+    apply Matrix.dotProduct_eq
+    intro w
+    rw [hAU w]
+    finishit
+  · intro ⟨y, hAy, hby⟩
+    use -y
+    constructor
+    · simpa [Matrix.mulVecLin, Matrix.neg_mulVec] using hAy
+    · simpa [Matrix.mulVecLin] using hby
+  · intro ⟨x, hAx, hbx⟩
+    use -x
+    constructor
+    · rw [Matrix.neg_mulVec_neg]
+      simpa [Matrix.mulVecLin] using hAx
+    · simpa [Matrix.mulVecLin] using hbx
+
+theorem mainFarkas (A : Matrix I J ℚ) (b : I → ℚ) :
+    (∃ x : J → ℚ, 0 ≤ x ∧ A *ᵥ x ≤ b) ≠ (∃ y : I → ℚ, 0 ≤ y ∧ -Aᵀ *ᵥ y ≤ 0 ∧ b ⬝ᵥ y < 0) := by
   sorry
+
+end corollaries
