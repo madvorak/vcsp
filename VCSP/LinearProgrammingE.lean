39e59393046489242a701ee6754b3f28abeeeed1
madvorak <martin.dvorak@matfyz.cz>
2024-06-10 15:30:31 +0200

    ExtendedLP.optimum

--- VCSP/LinearProgrammingE.lean
+++ VCSP/LinearProgrammingE.lean
@@ -28,23 +28,34 @@ def ExtendedLP.Reaches (P : ExtendedLP I J) (v : ℚ∞) : Prop :=
 
 /-- Linear program `P` is feasible iff there exists a vector `x` that is a solution to `P`. -/
 def ExtendedLP.IsFeasible (P : ExtendedLP I J) : Prop :=
-  ∃ x : J → ℚ, P.IsSolution x
+  ∃ x : J → ℚ, P.IsSolution x -- Does it even mean anything? What if `P.c ᵥ⬝ x = ⊥` here?
 
-/-- Linear program `P` is bounded iff there exists an upper bound on values reached by `P`. -/
+/-- Linear program `P` is bounded by `r` iff all values reached by `P` are less or equal to `r`. -/
+def ExtendedLP.IsBoundedBy (P : ExtendedLP I J) (r : ℚ∞) : Prop :=
+  ∀ p : ℚ∞, P.Reaches p → p ≤ r
+
+/-- Linear program `P` is bounded iff there exists a finite upper bound on values reached by `P`. -/
 def ExtendedLP.IsBounded (P : ExtendedLP I J) : Prop :=
-  ∃ r : ℚ∞, ∀ p : ℚ∞, P.Reaches p → p ≤ r
+  ∃ r : ℚ, P.IsBoundedBy r.toERat
 
 /-- Note that `ExtendedLP.dualize` is significantly different from `StandardLP.dualize`;
     here we keep maximizing but the sign is flipped. -/
 def ExtendedLP.dualize (P : ExtendedLP I J) : ExtendedLP J I :=
   ⟨-P.Aᵀ, -P.c, -P.b⟩
 
+lemma ExtendedLP.dualize_dualize (P : ExtendedLP I J) : P.dualize.dualize = P := by
+  obtain ⟨A, b, c⟩ := P
+  simp [ExtendedLP.dualize, ←Matrix.ext_iff]
+
 open Classical
 
 noncomputable def ExtendedLP.optimum (P : ExtendedLP I J) : Option ℚ∞ :=
   if P.IsFeasible then
     if P.IsBounded then
-      none -- TODO `some (sSup P.Reaches)` if it exists, otherwise `none` (invalid finite value)
+      if hr : ∃ r : ℚ, P.Reaches r.toERat ∧ P.IsBoundedBy r.toERat then
+        hr.choose -- the "maximum"
+      else
+        none -- invalid "finite" value
     else
       some ⊤
   else
