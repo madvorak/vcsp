32187e12d6485ab179c519532c527b7810183540
madvorak <martin.dvorak@matfyz.cz>
2024-07-19 08:52:36 +0200

    Rouché-Frobenius theorem as a corollary

--- VCSP/FarkasBasic.lean
+++ VCSP/FarkasBasic.lean
@@ -100,3 +100,48 @@ theorem inequalityFarkas [DecidableEq I] (A : Matrix I J F) (b : I → F) :
     (∃ x : J → F, 0 ≤ x ∧ A *ᵥ x ≤ b) ≠ (∃ y : I → F, 0 ≤ y ∧ 0 ≤ Aᵀ *ᵥ y ∧ b ⬝ᵥ y < 0) := by
   convert inequalityFarkas_neg A b using 5
   convertit
+
+lemma equalityFrobenius_lt (A : Matrix I J F) (b : I → F) :
+    (∃ x : J → F, A *ᵥ x = b) ≠ (∃ y : I → F, Aᵀ *ᵥ y = 0 ∧ b ⬝ᵥ y < 0) := by
+  convert equalityFarkas (Matrix.fromColumns A (-A)) b using 1
+  · constructor
+    · intro ⟨x, hAx⟩
+      use Sum.elim x⁺ x⁻
+      constructor
+      · rw [Sum.nonneg_elim_iff]
+        exact ⟨posPart_nonneg x, negPart_nonneg x⟩
+      · rw [Matrix.fromColumns_mulVec_sum_elim, Matrix.neg_mulVec, ←Matrix.mulVec_neg, ←Matrix.mulVec_add, ←sub_eq_add_neg]
+        convert hAx
+        aesop
+    · intro ⟨x, _, hAx⟩
+      use x ∘ Sum.inl - x ∘ Sum.inr
+      rw [Matrix.mulVec_sub]
+      rwa [←Sum.elim_comp_inl_inr x, Matrix.fromColumns_mulVec_sum_elim, Matrix.neg_mulVec, ←sub_eq_add_neg] at hAx
+  · constructor
+    · intro ⟨y, hAy, hby⟩
+      refine ⟨y, ?_, hby⟩
+      rw [Matrix.transpose_fromColumns, Matrix.fromRows_mulVec, Sum.nonneg_elim_iff]
+      constructor
+      · rw [hAy]
+      · rw [Matrix.transpose_neg, Matrix.neg_mulVec, hAy, neg_zero]
+    · intro ⟨y, hAy, hby⟩
+      refine ⟨y, ?_, hby⟩
+      rw [Matrix.transpose_fromColumns, Matrix.fromRows_mulVec, Sum.nonneg_elim_iff] at hAy
+      obtain ⟨hAyp, hAyn⟩ := hAy
+      refine eq_of_le_of_le ?_ hAyp
+      intro i
+      specialize hAyn i
+      rwa [Matrix.transpose_neg, Matrix.neg_mulVec, Pi.zero_apply, Pi.neg_apply, Right.nonneg_neg_iff] at hAyn
+
+theorem equalityFrobenius (A : Matrix I J F) (b : I → F) :
+    (∃ x : J → F, A *ᵥ x = b) ≠ (∃ y : I → F, Aᵀ *ᵥ y = 0 ∧ b ⬝ᵥ y ≠ 0) := by
+  convert equalityFrobenius_lt A b using 1
+  refine ⟨?_, by aesop⟩
+  intro ⟨y, hAy, hby⟩
+  if hlt : b ⬝ᵥ y < 0 then
+    aesop
+  else
+    use -y
+    rw [Matrix.mulVec_neg, hAy, neg_zero, Matrix.dotProduct_neg, neg_lt_zero]
+    push_neg at hlt
+    exact ⟨rfl, lt_of_le_of_ne hlt hby.symm⟩
