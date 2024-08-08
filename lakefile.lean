9b9c32abbc4d610158b4cdce1689695e58a60042
madvorak <martin.dvorak@matfyz.cz>
2024-08-08 10:01:15 +0200

    config

--- lakefile.lean
+++ lakefile.lean
@@ -2,14 +2,14 @@ import Lake
 open Lake DSL
 
 package vcsp {
-  moreServerOptions := #[⟨`autoImplicit, false⟩, ⟨`pp.structureInstances, false⟩, ⟨`linter.oldObtain, false⟩]
+  leanOptions := #[⟨`autoImplicit, false⟩, ⟨`pp.structureInstances, false⟩, ⟨`linter.oldObtain, false⟩]
 }
 
 require duality from git
   "https://github.com/madvorak/duality.git" @ "main"
+--require "madvorak" / "duality" @ git "main"
 
 @[default_target]
 lean_lib VCSP {
   globs := #[.submodules `VCSP]
-  moreLeanArgs := #["-DautoImplicit=false"]
 }
