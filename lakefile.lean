fe7819c0f2650a5f4f44b7684473ea5f54125995
Martin Dvorak <martin.dvorak@matfyz.cz>
2026-02-21 17:30:02 +0100

    bump duality

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package vcsp {
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`pp.structureInstances, false⟩, ⟨`linter.oldObtain, false⟩]
 }
 
-require "madvorak" / "duality" @ git "v3.3.0"
+require "madvorak" / "duality" @ git "v3.4.0"
 
 @[default_target]
 lean_lib VCSP {
